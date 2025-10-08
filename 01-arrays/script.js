7225a596c49dd1eeb22f408d706f174e4046faf1
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-08 16:50:30 +0200

    reset part of exercise of base document and go to bonus to work with the for loop

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -41,65 +41,35 @@ if (lastTeacher = "Luca") {
 
 console.log(teachers);
 console.log(lastTeacher);
-
-
 // const lastTeacher = teachers.pop();
 // console.log(teachers);
 // console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 
-
-
-
-
-// const firstTeacher = teachers.shift();
-// console.log(teachers);
-// console.log(firstTeacher)
+const firstTeacher = teachers.shift();
+console.log(teachers);
+console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
-
-
-
-
-
-
-
-// teachers.push("Vanessa")
-// console.log(teachers)
+teachers.push("Vanessa")
+console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
-
-
-
-
-
-// teachers.unshift("Sarah")
-// console.log(teachers)
+teachers.unshift("Sarah")
+console.log(teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 
-
-
-
-
-
-
-
-// const lewisIndex = teachers.indexOf("Lewis");
-// console.log(lewisIndex);
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
 
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 
-
-
-
-
-
-// const isTeachersEmpty = teachers.length == 0;
-// console.log(isTeachersEmpty);
+const isTeachersEmpty = teachers.length == 0;
+console.log(isTeachersEmpty);
